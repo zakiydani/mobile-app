e2e35453933352dcb8cd4e2f77fa79d4a8b22672
zakiydani <zakiydani87@gmail.com>
2023-10-17 14:06:14 +0700

    chore: add static data news

---
+++ src/data/index.ts
@@ -0,0 +1,74 @@
+import { INews } from '../interfaces'
+
+export const dataNews: INews[] = [
+  {
+    title: 'Andrée-Ann Labranche',
+    description:
+      'We must believe that we are gifted for something, and that this thing, at whatever cost, must be attained.',
+    createdDate: '2001-02-23',
+    profileImage: '',
+    newsImage: '',
+    comment: 256,
+    like: 4,
+  },
+  {
+    title: 'Adrien Stone',
+    description:
+      'If you’re offered a seat on a rocket ship, don’t ask what seat! Just get on.',
+    createdDate: '',
+    profileImage: '',
+    newsImage: '',
+    comment: 2,
+    like: 4353,
+  },
+  {
+    title: 'Andrée-Ann Labranche',
+    description:
+      'We must believe that we are gifted for something, and that this thing, at whatever cost, must be attained.',
+    createdDate: '',
+    profileImage: '',
+    newsImage: '',
+    comment: 0,
+    like: 43314,
+  },
+  {
+    title: 'Adrien Stone',
+    description:
+      'If you’re offered a seat on a rocket ship, don’t ask what seat! Just get on.',
+    createdDate: '',
+    profileImage: '',
+    newsImage: '',
+    comment: 21,
+    like: 21,
+  },
+  {
+    title: 'Andrée-Ann Labranche',
+    description:
+      'We must believe that we are gifted for something, and that this thing, at whatever cost, must be attained.',
+    createdDate: '',
+    profileImage: '',
+    newsImage: '',
+    comment: 12,
+    like: 566,
+  },
+  {
+    title: 'Adrien Stone',
+    description:
+      'If you’re offered a seat on a rocket ship, don’t ask what seat! Just get on.',
+    profileImage: '',
+    createdDate: '',
+    newsImage: '',
+    comment: 56,
+    like: 12,
+  },
+  {
+    title: 'Andrée-Ann Labranche',
+    description:
+      'We must believe that we are gifted for something, and that this thing, at whatever cost, must be attained.',
+    profileImage: '',
+    createdDate: '',
+    newsImage: '',
+    comment: 324,
+    like: 123,
+  },
+]
